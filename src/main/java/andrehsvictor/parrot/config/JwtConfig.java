04c71eff362699073032b71680bf2f93ee57b141
André Victor <andrehsvictor@gmail.com>
2024-07-21 20:38:06 -0300

    feat: Add JwtConfig for JWT configuration

---
+++ src/main/java/andrehsvictor/parrot/config/JwtConfig.java
@@ -0,0 +1,48 @@
+package andrehsvictor.parrot.config;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.validation.annotation.Validated;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import jakarta.validation.constraints.NotBlank;
+
+@Validated
+@Configuration
+public class JwtConfig {
+
+    @NotBlank
+    @Value("${parrot.rsa.private-key.path}")
+    private RSAPrivateKey privateKey;
+
+    @NotBlank
+    @Value("${parrot.rsa.public-key.path}")
+    private RSAPublicKey publicKey;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        JWKSet jwkSet = new JWKSet(jwk);
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(jwkSet);
+        return new NimbusJwtEncoder(jwkSource);
+    }
+}
