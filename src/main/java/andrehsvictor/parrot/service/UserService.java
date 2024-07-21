176c8f435c98b2415b0e1b2197c9bfd91465fe01
André Victor <andrehsvictor@gmail.com>
2024-07-21 15:16:59 -0300

    feat: Add method to find user by JwtAuthenticationToken

--- src/main/java/andrehsvictor/parrot/service/UserService.java
+++ src/main/java/andrehsvictor/parrot/service/UserService.java
@@ -1,6 +1,7 @@
 package andrehsvictor.parrot.service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import andrehsvictor.parrot.entity.User;
@@ -17,5 +18,10 @@ public class UserService {
     public User findByUsernameOrEmail(String usernameOrEmail) {
         return userRepository.findByUsernameOrEmail(usernameOrEmail)
                 .orElseThrow(() -> new ParrotException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    public User findByJwtAuthenticationToken(JwtAuthenticationToken jwt) {
+        String usernameOrEmail = jwt.getName();
+        return findByUsernameOrEmail(usernameOrEmail);
     }
 }
