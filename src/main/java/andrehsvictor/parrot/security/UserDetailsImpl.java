413421a4f03b76b52d9c209a8b2fbcc1fd5649af
André Victor <andrehsvictor@gmail.com>
2024-07-21 14:59:18 -0300

    feat: Add UserDetailsImpl for custom user details implementation

---
+++ src/main/java/andrehsvictor/parrot/security/UserDetailsImpl.java
@@ -0,0 +1,38 @@
+package andrehsvictor.parrot.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import andrehsvictor.parrot.entity.User;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getVerified();
+    }
+
+}
